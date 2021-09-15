1f7ba9f33dc7d2ff524316d9aa1e4e57fefac4e3
Marcus Fulton <marcusfulton11@yahoo.com>
2021-09-15 20:24:40 +0100

    form with errors (not fully functional)

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,15 +1,43 @@
 import  React, { useState } from "react";
 
 
-export default function LoginForm() {
-    const [purchasePrice, setPurchasePrice] = useState("");
-    const [make, setMake] = useState("");
+export default function LoginForm() { //Creates function for login form
+    const [purchasePrice, setPurchasePrice] = useState(""); //sets inital state of purchase price
+    const [make, setMake] = useState(""); //same as above for make
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(purchasePrice, make)
+    const [purchasePriceErr, setPurchasePriceErr] = useState({});
+    const [makeErr, setMakeErr] = useState({});
+
+    const handleSubmit = (e) => { // function to handle the submit button when clicked
+        e.preventDefault(); // a preventDefault is called on the event when submitting the form to prevent the browser reload/refresh
+        const isValid = formValidation();
+        if(isValid){
+            // move to acceptance page
+        } else {
+            // move to rejection page
+        }
     }
   
+    const formValidation = () =>{
+        const purchasePriceErr = {};
+        const makeErr = {};
+        let isValid = true;
+        
+        if(purchasePrice.length > 10) {
+            purchasePriceErr.highNumber = "Error Please enter valid purchase price"
+            isValid = false;
+        }
+
+        if(make.length > 10) {
+            makeErr.highLetterVal = "Error enter a valid make"
+            isValid = false;
+        }
+
+        setPurchasePriceErr(purchasePrice);
+        setMakeErr(make);
+        return isValid;
+    
+    }
 
     return (
       <form onSubmit={handleSubmit}>
@@ -24,7 +52,10 @@ export default function LoginForm() {
           onChange={(e) => setPurchasePrice(e.target.value)}
         />
         </div>
-        <p>{purchasePrice}</p>
+        {Object.keys(purchasePriceErr).map((key)=>{
+            return <div style={{color: "red"}}>{purchasePriceErr[key]}</div>
+        })}
+        {/* <p>{purchasePrice}</p> */}
         <br/>
         <div>
         <label>Make</label>
@@ -38,8 +69,11 @@ export default function LoginForm() {
         />
         </div>
         <br/>
-        <p>{make}</p>
+        {Object.keys(makeErr).map((key)=>{
+            return <div style={{color: "red"}}>{makeErr[key]}</div>
+        })}
+        {/* <p>{make}</p> */}
         <button type="submit">Submit </button>
       </form>
     );
-  }
+  } 
