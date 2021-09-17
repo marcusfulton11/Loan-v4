37650a3883e66db63f831cef20ca128bd9e6eb3f
Marcus Fulton <marcusfulton11@yahoo.com>
2021-09-17 12:58:20 +0100

    Form with working errors

---
+++ src/Components/Rejection/Rejection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './Components/Form/Form';
+
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+
+  );
+}
+
+export default App;
