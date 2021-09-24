5548e226981c7aeccfc5e5b580d38a59d5095509
Marcus Fulton <marcusfulton11@yahoo.com>
2021-09-24 20:25:50 +0100

    created signup page

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,53 @@
+import  React, { useState } from "react";
+
+
+ 
+
+export default function SignUp() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const [emailErr, setEmailErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [confirmPasswordErr, setConfirmPasswordErr] = useState("");
+
+  return (
+    <form onSubmit={handleSubmit}>
+     <div>
+       <label>Email address</label>
+        <input type="email"  
+               name="email" 
+               placeholder="Enter email"
+               value={email}
+        />
+       </div>
+      <div>
+         <label>Password</label>
+          <input type="password"
+                 name="password" 
+                 placeholder="Password"
+                 value={password}
+        />
+        </div>
+      <div>
+          <label>Confirm Password</label>
+           <input type="password"  
+                  id="confirmPassword" 
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+        />
+      </div>
+        <button 
+           type="submit" 
+                >
+            Register
+                </button>
+            </form>
+  );
+}
